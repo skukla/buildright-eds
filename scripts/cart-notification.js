0009cc4d7d414a92c6fc94ce1989b8ded47baa83
Steve Kukla <kukla@adobe.com>
2025-12-06 20:01:01 -0500

    style: update cart notification toast styling
    - Update checkmark icon SVG size and stroke width
    - (CSS updates included in Card Tile commit)

--- scripts/cart-notification.js
+++ scripts/cart-notification.js
@@ -24,8 +24,8 @@ export function showCartNotification(productName, quantity = 1) {
   // Build notification content
   notification.innerHTML = `
     <div class="cart-notification-icon">
-      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20 6L9 17L4 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 6L9 17L4 12" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </div>
     <div class="cart-notification-content">
